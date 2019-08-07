1f1a627abba640cb7cb3ffded45bf88341440fa1
ComradeYanis <wot2304@gmail.com>
2019-08-07 16:28:34 +0300

    add controllers

---
+++ app/code/Maxime/Jobs/Controller/Adminhtml/Department/Index.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Maxime\Jobs\Controller\Adminhtml\Department;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ * @package Maxime\Jobs\Controller\Adminhtml\Department
+ */
+class Index extends Action
+{
+
+    /**
+     * @const ADMIN_RESOURCE
+     */
+    const ADMIN_RESOURCE = 'Maxime_Jobs::department';
+
+    /**
+     * @var PageFactory $resultPageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Maxime_Jobs::department');
+        $resultPage->addBreadcrumb(__('Jobs'), __('Jobs'));
+        $resultPage->addBreadcrumb(__('Manage Departments'), __('Manage Departments'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Department'));
+
+        return $resultPage;
+    }
+}
