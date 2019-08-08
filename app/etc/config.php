14d4361abede4a4cd427d9d93bc8e1aaf6045e63
ComradeYanis <wot2304@gmail.com>
2019-08-08 11:41:29 +0300

    add action controllers

--- app/etc/config.php
+++ app/etc/config.php
@@ -213,7 +213,6 @@ return [
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
         'Maxime_Helloworld' => 1,
-        'Maxime_Jobs' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
     ]
