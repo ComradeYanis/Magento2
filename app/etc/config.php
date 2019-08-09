7977ee51aaa0535e643106dde95df7437eeb1012
ComradeYanis <wot2304@gmail.com>
2019-08-09 11:15:45 +0300

    minor fix

--- app/etc/config.php
+++ app/etc/config.php
@@ -213,6 +213,7 @@ return [
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
         'Maxime_Helloworld' => 1,
+        'Maxime_Jobs' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
     ]
