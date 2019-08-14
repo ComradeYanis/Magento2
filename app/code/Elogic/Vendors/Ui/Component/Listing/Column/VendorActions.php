62f01c7c5b22c2debc47e235202710e323c74e9a
ComradeYanis <wot2304@gmail.com>
2019-08-14 17:18:57 +0300

    add VendorActions

---
+++ app/code/Elogic/Vendors/Ui/Component/Listing/Column/VendorActions.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Elogic\Vendors\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class VendorActions
+ * @package Elogic\Vendors\Ui\Component\Listing\Column
+ */
+class VendorActions extends Column
+{
+    /**
+     * @var UrlInterface $urlBuilder
+     */
+    protected $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'vendors/vendor/edit',
+                        ['id' => $item['entity_id']]
+                    ),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+                $item[$this->getData('name')]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'vendors/vendor/delete',
+                        ['id' => $item['entity_id']]
+                    ),
+                    'label' => __('Delete'),
+                    'hidden' => false,
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
