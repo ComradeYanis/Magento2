5a9f69581981ab6a4fcff685da0cb51274b7934b
ComradeYanis <wot2304@gmail.com>
2019-08-23 03:24:21 +0300

    done file upload helper

--- app/code/Elogic/Vendors/Controller/Adminhtml/Vendor/Save.php
+++ app/code/Elogic/Vendors/Controller/Adminhtml/Vendor/Save.php
@@ -2,19 +2,15 @@
 
 namespace Elogic\Vendors\Controller\Adminhtml\Vendor;
 
+use Elogic\Vendors\Helper\UploadPhoto;
 use Elogic\Vendors\Model\VendorRepository;
 use Exception;
 use Magento\Backend\App\Action;
 use Magento\Backend\Model\View\Result\Redirect;
-use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Filesystem;
-use Magento\Framework\Image\AdapterFactory;
-use Magento\Framework\UrlInterface;
-use Magento\MediaStorage\Model\File\UploaderFactory;
 use Magento\Store\Model\StoreManagerInterface;
 use RuntimeException;
 
@@ -32,54 +28,29 @@ class Save extends Action
     protected $_modelRepository;
 
     /**
-     * @var UploaderFactory $_uploader
-     */
-    protected $_uploader;
-
-    /**
-     * @var AdapterFactory $_adapterFactory
-     */
-    protected $_adapterFactory;
-
-    /**
-     * @var Filesystem $_fileSystem
-     */
-    protected $_fileSystem;
-
-    /**
-     * @var Filesystem\Driver\File $_file
-     */
-    protected $_file;
-
-    /**
      * @var StoreManagerInterface $_storeManager
      */
     protected $_storeManager;
+
+    /**
+     * @var UploadPhoto $_uploadPhotoHelper
+     */
+    protected $_uploadPhotoHelper;
 
     //endregion
 
     /**
      * @param Action\Context $context
      * @param VendorRepository $modelRepository
-     * @param UploaderFactory $uploader
-     * @param AdapterFactory $adapterFactory
-     * @param Filesystem $filesystem
-     * @param Filesystem\Driver\File $file
-     * @param StoreManagerInterface $storeManager
+     * @param UploadPhoto $uploadPhotoHelper
      */
     public function __construct(
         Action\Context $context,
         VendorRepository $modelRepository,
-        UploaderFactory $uploader,
-        AdapterFactory $adapterFactory,
-        Filesystem $filesystem,
-        Filesystem\Driver\File $file
+        UploadPhoto $uploadPhotoHelper
     ) {
-        $this->_uploader = $uploader;
-        $this->_adapterFactory = $adapterFactory;
-        $this->_fileSystem = $filesystem;
         $this->_modelRepository = $modelRepository;
-        $this->_file = $file;
+        $this->_uploadPhotoHelper = $uploadPhotoHelper;
         parent::__construct($context);
     }
 
@@ -112,7 +83,12 @@ class Save extends Action
                 $model = $this->_modelRepository->create();
             }
 
-            $data['logo'] = $this->uploadPhoto($data);
+            $file = $this->_request->getFiles('logo');
+            if (isset($file) && isset($file['name']) && strlen($file['name'])) {
+
+                $data['logo'] = $this->_uploadPhotoHelper
+                    ->uploadPhoto($data, 'logo', $file, $this->_modelRepository::BASE_MEDIA_PATH);
+            }
 
             $this->_eventManager->dispatch(
                 'vendors_vendor_prepare_save',
@@ -141,56 +117,5 @@ class Save extends Action
             return $resultRedirect->setPath('*/*/edit', ['entity_id' => $this->getRequest()->getParam('id')]);
         }
         return $resultRedirect->setPath('*/*/');
-    }
-
-    /**
-     * @param array $data
-     * @return string|null
-     * @throws FileSystemException
-     * @throws NoSuchEntityException
-     */
-    protected function uploadPhoto(array $data)
-    {
-        $file = $this->_request->getFiles('logo');
-        $result = null;
-
-        if (isset($file) && isset($file['name']) && strlen($file['name'])) {
-            try {
-                $uploader = $this->_uploader->create(['fileId' => 'logo']);
-                $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
-                $imageAdapter = $this->_adapterFactory->create();
-
-                $uploader->addValidateCallback('logo', $imageAdapter, 'validateUploadFile');
-                $uploader->setAllowRenameFiles(true);
-                $uploader->setFilesDispersion(true);
-
-                $mediaDirectory = $this->_fileSystem->getDirectoryRead(DirectoryList::MEDIA);
-                $result = $uploader->save($mediaDirectory->getAbsolutePath($this->_modelRepository::BASE_MEDIA_PATH));
-
-                $result = $this->_modelRepository::BASE_MEDIA_PATH . $result['file'];
-            } catch (Exception $e) {
-                $this->messageManager->addError($e->getMessage());
-            }
-        } else {
-            if (isset($data['logo']) && isset($data['logo']['value'])) {
-                if (isset($data['logo']['delete'])) {
-                    $data['logo'] = null;
-                    $data['delete_logo'] = true;
-//                    $fullFilePath = $this->_storeManager->getStore()->getBaseUrl(UrlInterface::URL_TYPE_MEDIA) . $data['logo']['value'];
-                    $fullFilePath = $data['logo']['value'];
-                    if ($this->_file->isExists($fullFilePath)) {
-                        try {
-                            $this->_file->deleteFile($fullFilePath);
-                        } catch (Exception $e) {
-                            $this->messageManager->addError($e->getMessage());
-                        }
-                    }
-                } elseif (isset($data['logo']['value'])) {
-                    $result = $data['logo']['value'];
-                }
-            }
-        }
-
-        return $result;
     }
 }
--- app/code/Elogic/Vendors/Helper/UploadPhoto.php
+++ app/code/Elogic/Vendors/Helper/UploadPhoto.php
@@ -4,7 +4,7 @@ namespace Elogic\Vendors\Helper;
 
 use Exception;
 use Magento\Framework\App\Filesystem\DirectoryList;
-use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Filesystem;
 use Magento\Framework\Image\AdapterFactory;
 use Magento\MediaStorage\Model\File\UploaderFactory;
@@ -37,22 +37,30 @@ class UploadPhoto
     protected $_fileSystem;
 
     /**
+     * @var Filesystem\Driver\File $_file
+     */
+    protected $_file;
+
+    /**
      * UploadPhoto constructor.
      * @param StoreManagerInterface $storeManager
      * @param UploaderFactory $uploader
      * @param AdapterFactory $adapterFactory
      * @param Filesystem $filesystem
+     * @param Filesystem\Driver\File $file
      */
     public function __construct(
         StoreManagerInterface $storeManager,
         UploaderFactory $uploader,
         AdapterFactory $adapterFactory,
-        Filesystem $filesystem
+        Filesystem $filesystem,
+        Filesystem\Driver\File $file
     ) {
         $this->_storeManager    = $storeManager;
         $this->_uploader        = $uploader;
         $this->_adapterFactory  = $adapterFactory;
         $this->_fileSystem      = $filesystem;
+        $this->_file            = $file;
     }
 
     /**
@@ -62,21 +70,16 @@ class UploadPhoto
      * @param string $mediaPath
      * @param string $fileDirectory
      * @return mixed|string
-     * @throws NoSuchEntityException
+     * @throws FileSystemException
+     * @throws Exception
      */
     public function uploadPhoto(array $data, string $photoAttributeName, array $requestFile, string $mediaPath, string $fileDirectory = DirectoryList::MEDIA)
     {
+        $result = null;
+
         if (isset($requestFile) && isset($requestFile['name']) && strlen($requestFile['name'])) {
             try {
-                $uploader = $this->_uploader->create(['fileId' => $photoAttributeName]);
-                $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
-                $imageAdapter = $this->_adapterFactory->create();
-
-                $uploader->addValidateCallback('logo', $imageAdapter, 'validateUploadFile');
-                $uploader->setAllowRenameFiles(true);
-                $uploader->setFilesDispersion(true);
-
-                $result = $uploader->save($this->getMediaPath($fileDirectory)->getAbsolutePath($mediaPath));
+                $result = $this->saveFile($photoAttributeName, $mediaPath, $fileDirectory);
 
                 $result = $mediaPath . $result['file'];
             } catch (Exception $e) {
@@ -85,16 +88,8 @@ class UploadPhoto
         } else {
             if (isset($data['logo']) && isset($data['logo']['value'])) {
                 if (isset($data['logo']['delete'])) {
-                    $data['logo'] = null;
-                    $data['delete_logo'] = true;
-                    $fullFilePath = $this->getMediaPath($fileDirectory)->getAbsolutePath($data['logo']['value'])
-                    if ($this->_file->isExists($fullFilePath)) {
-                        try {
-                            $this->_file->deleteFile($fullFilePath);
-                        } catch (Exception $e) {
-                            throw new Exception($e->getMessage());
-                        }
-                    }
+                    $fullFilePath = $this->getFileFullPath($data['logo']['value'], $fileDirectory);
+                    $this->deleteFile($fullFilePath);
                 } elseif (isset($data['logo']['value'])) {
                     $result = $data['logo']['value'];
                 }
@@ -111,4 +106,61 @@ class UploadPhoto
     protected function getMediaPath(string $baseFilePath)
     {
         return $this->_fileSystem->getDirectoryRead($baseFilePath);
-    }}
+    }
+
+    /**
+     * @param string $filePath
+     * @param string $fileDirectory
+     * @return string
+     */
+    protected function getFileFullPath(string $filePath, string $fileDirectory = DirectoryList::MEDIA)
+    {
+        return $this->getMediaPath($fileDirectory)->getAbsolutePath($filePath);
+    }
+
+    /**
+     * @param string $fullFilePath
+     * @return bool
+     * @throws FileSystemException
+     */
+    protected function fileExist(string $fullFilePath)
+    {
+        return $this->_file->isExists($fullFilePath);
+    }
+
+    /**
+     * @param string $fullFilePath
+     * @throws FileSystemException
+     * @throws Exception
+     */
+    protected function deleteFile(string $fullFilePath)
+    {
+        if ($this->fileExist($fullFilePath)) {
+            try {
+                $this->_file->deleteFile($fullFilePath);
+            } catch (Exception $e) {
+                throw new Exception($e->getMessage());
+            }
+        }
+    }
+
+    /**
+     * @param string $photoAttributeName
+     * @param string $mediaPath
+     * @param string $fileDirectory
+     * @return array
+     * @throws Exception
+     */
+    protected function saveFile(string $photoAttributeName, string $mediaPath, string $fileDirectory = DirectoryList::MEDIA)
+    {
+        $uploader = $this->_uploader->create(['fileId' => $photoAttributeName]);
+        $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
+        $imageAdapter = $this->_adapterFactory->create();
+
+        $uploader->addValidateCallback('logo', $imageAdapter, 'validateUploadFile');
+        $uploader->setAllowRenameFiles(true);
+        $uploader->setFilesDispersion(true);
+
+        return $uploader->save($this->getFileFullPath($mediaPath, $fileDirectory));
+    }
+}
