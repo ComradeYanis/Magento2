a16889f36e4b98129d4734c920c2c4589a9aab34
ComradeYanis <wot2304@gmail.com>
2019-09-04 17:19:27 +0300

    test observable array

--- app/code/Elogic/Test/view/frontend/web/js/plain-view-model.js
+++ app/code/Elogic/Test/view/frontend/web/js/plain-view-model.js
@@ -1,19 +1,15 @@
 define(['ko', 'jquery'], function (ko, $) {
     'use strict';
 
-    return function (config) {
-        let currencyInfo = ko.observable();
-        $.getJSON(config.base_url + 'rest/V1/directory/currency', currencyInfo);
-
+    return function () {
         const viewModel = {
-            label: ko.observable('Test info')
+            exchange_rates: ko.observable([
+                {
+                    currency_to: 'USD',
+                    rate: 1.0
+                }
+            ])
         };
-        viewModel.output = ko.computed(function () {
-            if (currencyInfo()) {
-                return this.label() + ':\n' + JSON.stringify(currencyInfo(), null, 2);
-            }
-            return '...loading';
-        }.bind(viewModel));
         return viewModel;
     }
 });
